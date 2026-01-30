2c13e0381c60c9dd470a518241f8804b46f84b5e
karibbeanluxuryoperators-art <karibbeanluxuryoperators@gmail.com>
2026-01-29 20:32:37 -0500

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -12,8 +12,7 @@ export const NAV_ITEMS: NavItem[] = [
   { labelKey: 'nav.services', href: '#servicios' },
   { labelKey: 'nav.team', href: '#equipo' },
   { labelKey: 'nav.metrics', href: '#metricas' },
-  { labelKey: 'nav.experiences', href: '#experiencias' },
-  { labelKey: 'nav.investors', href: '#inversionistas'},
+{ labelKey: 'nav.experiences', href: '/experiencias' },  { labelKey: 'nav.investors', href: '#inversionistas'},
 ];
 
 export const DESTINATIONS: Destination[] = [
@@ -773,3 +772,4 @@ export const getTranslation = (key: string, lang: Language): any => {
   
   return value ?? key;
 };
+
