f124f4e686934c6e842afb0e5eaf6639738a2559
karibbeanluxuryoperators-art <karibbeanluxuryoperators@gmail.com>
2026-06-19 12:06:05 -0500

    Modify Tailwind config for content paths
    Updated content paths to include components and public directories.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
